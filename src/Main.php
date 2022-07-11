e86fbaf0131d3b25496bcd4b0e183f2086c05492
famima65536 <famima65536@gmail.com>
2022-07-11 11:49:59 +0900

    register StrictFormExampleCommand

--- src/Main.php
+++ src/Main.php
@@ -2,15 +2,14 @@
 
 namespace oirancage\strictformexample;
 
+use oirancage\strictformexample\command\StrictFormExampleCommand;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 	public function onEnable() : void{
-		$this->getServer()->getCommandMap()->registerAll(
+		$this->getServer()->getCommandMap()->register(
 			"strictformexample",
-			[
-
-			]
+			new StrictFormExampleCommand()
 		);
 	}
 }
